63326cc717536ca114b42f3ee8c0c759945a58a5
PIBO0121038 Фомин Иван Константинович <PIBO0121038@sibupk.su>
2023-01-16 14:59:26 +0700

    Добавьте файлы проекта.

---
+++ 9zadanie/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9zadanie
+{
+    class zadanie9
+    {
+        static void Main()
+        {
+            Console.Write("Ввести первое число a:");
+            var a = int.Parse(Console.ReadLine());
+            Console.Write("Ввести первое число b:");
+            var b = int.Parse(Console.ReadLine());
+            Console.Write("Ввести первое число c:");
+            var c = int.Parse(Console.ReadLine());
+            Console.WriteLine(a - 1);
+            Console.WriteLine(b);
+            Console.WriteLine(c + 1);
+            Console.ReadKey();
+        }
+    }
+}
